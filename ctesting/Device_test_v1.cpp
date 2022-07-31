f11def45594707db0c3a5d10ca88744bb6ac6638
Harsh Nannur <harsha2bits@gmail.com>
2022-07-31 04:51:27 -0500

    not working encoder logic

--- ctesting/Device_test_v1.cpp
+++ ctesting/Device_test_v1.cpp
@@ -24,16 +24,8 @@ Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
 #define ENCODER_SW  25
 
 int counter = 0;
-int position = 0; //defining position of stepper as 1,2,3
-
-
-
-
-
-
-
-
-
+int counterprev = 0;
+int position = 0; // defining position of stepper as 1,2,3
 
 //#include <stdio.h>
 using namespace std;
@@ -121,36 +113,15 @@ void readEncoder() {
   int dtValue = digitalRead(ENCODER_DT);
   if (dtValue == HIGH && position !=2 ) {
     counter++; // Clockwise
-    myStepper.step(counter * 4);
+    //myStepper.step(counter * 4);
   }
   if (dtValue == LOW && position !=0) {
     counter--; // Counterclockwise
-    myStepper.step(-counter * 4);
+    //myStepper.step(-counter * 4);
   }
 
 
-/*
-Each counter step is equal to 4 steps of stepper. 
-So to make 90 degree turn we need 512 steps which is 128 steps
- counter= 0 is position 0
- counter = 64 is pisition 1
- counter =128 is pisition 2
-*/
-  switch(counter){
-    case 0:
-      position = 0;
-    case 64:
-      position = 1;
-    case 128:
-      position = 2;
-    }
 
-/* send data to Hub about new position */
-  sendData.id = 2;
-  sendData.position = position;
-  sendData.steps = counter * 4;
-  sendData.status = "Manual";
-  send_data();
 
   
 }
@@ -241,76 +212,92 @@ myStepper.setSpeed(5);
 
 }
 
+int change = 0;
+int countertemp = 0;
+void loop()
+{
 
-
-
-
-void loop() {
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Manual Input Code */
-  if (Serial.available()) { // if there is data comming
-    String command = Serial.readStringUntil('\n'); // read string until newline character
-    Serial.println("THE INPUT IS " + command );
-
-    if (command == "open")
+    countertemp = getCounter();
+    if (countertemp!= counterprev)
     {
-      digitalWrite(LED_BUILTIN, HIGH); // turn on LED
-      myStepper.step(stepsPerRevolution/4);
-      Serial.println("Blinds open");
-      sendData.id = 1;
-      sendData.position = 0;
-      sendData.status = command;
-      send_data();
+        /*
+    Each counter step is equal to 4 steps of stepper.
+    So to make 90 degree turn we need 512 steps which is 128 steps
+     counter= 0 is position 0
+     counter = 64 is pisition 1
+     counter =128 is pisition 2
+    */
+        change = countertemp - counterprev;
+        myStepper.step(change * 4)
+
+            switch (countertemp)
+        {
+        case 0:
+            position = 0;
+        case 64:
+            position = 1;
+        case 128:
+            position = 2;
+        }
+
+        counterprev = countertemp;
+
+        /* send data to Hub about new position */
+        sendData.id = 2;
+        sendData.position = position;
+        sendData.steps = countertemp * 4;
+        sendData.status = "Manual";
+        send_data();
     }
-    else if (command == "close")
-    {
-      digitalWrite(LED_BUILTIN, LOW);  // turn off LED
-      myStepper.step(-stepsPerRevolution/4);
-      Serial.println("Blinds are closed");
-      sendData.id = 1;
-      sendData.position = 0;
-      sendData.status = command;
-      send_data();
-    }
-    else if (command == "manual")
-    {
-      
-      while(Serial.available())
-      {
-        Serial.println("Enter the position you want to set:");
-        String psts =Serial.readStringUntil('\n');
-        sendData.position = stoi("45");
-        
-      }
-      
-      for (int i = 0; i <= 20; i++ )
-      {
-        delay(200);
-        digitalWrite(LED_BUILTIN, LOW); // turn off LED
-        delay(200);
-        digitalWrite(LED_BUILTIN, HIGH); // turn off LED
-       } 
-      Serial.println("Blinds are closed");
-      sendData.id = 1;
-      sendData.status = command;
-      
-      send_data();
-      }
-  }
 
+    /* Manual Input Code */
+    if (Serial.available())
+    {                                                  // if there is data comming
+        String command = Serial.readStringUntil('\n'); // read string until newline character
+        Serial.println("THE INPUT IS " + command);
+
+        if (command == "open")
+        {
+            digitalWrite(LED_BUILTIN, HIGH); // turn on LED
+            myStepper.step(stepsPerRevolution / 4);
+            Serial.println("Blinds open");
+            sendData.id = 1;
+            sendData.position = 0;
+            sendData.status = command;
+            send_data();
+        }
+        else if (command == "close")
+        {
+            digitalWrite(LED_BUILTIN, LOW); // turn off LED
+            myStepper.step(-stepsPerRevolution / 4);
+            Serial.println("Blinds are closed");
+            sendData.id = 1;
+            sendData.position = 0;
+            sendData.status = command;
+            send_data();
+        }
+        else if (command == "manual")
+        {
+
+            while (Serial.available())
+            {
+                Serial.println("Enter the position you want to set:");
+                String psts = Serial.readStringUntil('\n');
+                sendData.position = stoi("45");
+            }
+
+            for (int i = 0; i <= 20; i++)
+            {
+                delay(200);
+                digitalWrite(LED_BUILTIN, LOW); // turn off LED
+                delay(200);
+                digitalWrite(LED_BUILTIN, HIGH); // turn off LED
+            }
+            Serial.println("Blinds are closed");
+            sendData.id = 1;
+            sendData.status = command;
+
+            send_data();
+        }
+    }
 }
-
-
-
