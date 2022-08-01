b2a529206a028fcd6b242a572ba68c1cf8aeae4c
Harsh Nannur <harsha2bits@gmail.com>
2022-08-01 17:55:34 -0500

    Closing all windows

---
+++ Stepper_Motor/AccelLibraryStep.cpp
@@ -0,0 +1,69 @@
+// MultiStepper.pde
+// -*- mode: C++ -*-
+//
+// Shows how to multiple simultaneous steppers
+// Runs one stepper forwards and backwards, accelerating and decelerating
+// at the limits. Runs other steppers at the same time
+//
+// Copyright (C) 2009 Mike McCauley
+// $Id: MultiStepper.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
+
+#include <AccelStepper.h>
+
+// Define some steppers and the pins the will use
+AccelStepper stepper1; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+AccelStepper stepper2(AccelStepper::FULL4WIRE, 6, 7, 8, 9);
+AccelStepper stepper3(AccelStepper::FULL2WIRE, 10, 11);
+
+void setup()
+{  
+    stepper1.setMaxSpeed(200.0);
+    stepper1.setAcceleration(100.0);
+    stepper1.moveTo(24);
+    
+    stepper2.setMaxSpeed(300.0);
+    stepper2.setAcceleration(100.0);
+    stepper2.moveTo(1000000);
+    
+    stepper3.setMaxSpeed(300.0);
+    stepper3.setAcceleration(100.0);
+    stepper3.moveTo(1000000); 
+}
+
+void loop()
+{
+    // Change direction at the limits
+    if (stepper1.distanceToGo() == 0)
+	stepper1.moveTo(-stepper1.currentPosition());
+    stepper1.run();
+    stepper2.run();
+    stepper3.run();
+}
+
+
+// Include the AccelStepper Library
+#include <AccelStepper.h>
+
+// Define step constant
+#define FULLSTEP 4
+
+// Creates an instance
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+AccelStepper myStepper(FULLSTEP, 8, 10, 9, 11);
+
+void setup() {
+	// set the maximum speed, acceleration factor,
+	// initial speed and the target position
+	myStepper.setMaxSpeed(1000.0);
+	myStepper.setAcceleration(50.0);
+	myStepper.setSpeed(200);
+	myStepper.moveTo(2038);
+}
+
+void loop() {
+	// Change direction once the motor reaches target position
+	if (myStepper.distanceToGo() == 0) 
+		myStepper.moveTo(-myStepper.currentPosition());
+
+	// Move the motor one step
+	myStepper.run();
